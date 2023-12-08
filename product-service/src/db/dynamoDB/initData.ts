2c22ae7158332da3d509cd9b82776938b0b4cb46
Andrii Gromakov <andriigromakov@gmail.com>
2023-12-08 09:56:58 +0200

    fix(task-6): fixed env vars

--- product-service/src/db/dynamoDB/initData.ts
+++ product-service/src/db/dynamoDB/initData.ts
@@ -1,8 +1,7 @@
 import { DynamoDBClient, TransactWriteItemsCommand } from "@aws-sdk/client-dynamodb";
 import { marshall } from "@aws-sdk/util-dynamodb";
-
+import 'dotenv/config';
 import { stocks, products } from '../../mocks/data'
-
 
 const client = new DynamoDBClient({
   region: process.env.PRODUCT_AWS_REGION,
