073fdd920d4633b9e8db72fa45868bd66b8d74a3
Andrii Gromakov <andriigromakov@gmail.com>
2023-12-11 14:29:37 +0200

    fix: tmp auth fix

--- import-service/cdk.ts
+++ import-service/cdk.ts
@@ -5,7 +5,6 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3notifications from 'aws-cdk-lib/aws-s3-notifications';
 import * as apiGateway from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
-// import { HttpLambdaAuthorizer, HttpLambdaResponseType } from 'aws-cdk-lib/aws-apigatewayv2-authorizers';
 import * as sqs from "aws-cdk-lib/aws-sqs";
 import 'dotenv/config';
 const BASE_URL = 'import';
@@ -16,12 +15,7 @@ const stack = new cdk.Stack(app, "ImportServiceStack", {
   env: { region: process.env.PRODUCT_AWS_REGION },
 });
 
-const productQueue = sqs.Queue.fromQueueArn(stack, 'ProductQueue', process.env.PRODUCT_QUEUE_ARN!)
-// const authorizerFunction = lambda.Function.fromFunctionArn(stack, 'AuthorizerFunction', process.env.AUTHORIZER_FUNCTION_ARN!);
-
-// const authorizer = new HttpLambdaAuthorizer('BasicAuthorizer', authorizerFunction, {
-//   responseTypes: [HttpLambdaResponseType.IAM],
-// });
+const productQueue = sqs.Queue.fromQueueArn(stack, 'ProductQueue', process.env.PRODUCT_QUEUE_ARN!);
 
 const bucket = new s3.Bucket(stack, "ImportBucket", {
   bucketName: "import-bucket-asgrm",
@@ -56,11 +50,12 @@ const importProductsFile = new NodejsFunction(stack, 'ImportProductsFileLambda',
   entry: 'src/handlers/importProductsFile/app.ts',
   functionName: 'importProductsFile',
 });
-// const authTest = new NodejsFunction(stack, 'AuthTestLambda', {
-//   ...sharedLambdaProps,
-//   entry: 'src/handlers/authTest/app.ts',
-//   functionName: 'authTest',
-// });
+
+const authTest = new NodejsFunction(stack, 'AuthTestLambda', {
+  ...sharedLambdaProps,
+  entry: 'src/handlers/authTest/app.ts',
+  functionName: 'authTest',
+});
 
 const importFileParser = new NodejsFunction(stack, 'ImportFileParser Lambda', {
   runtime: lambda.Runtime.NODEJS_18_X,
@@ -97,12 +92,19 @@ api.addRoutes({
   integration: new HttpLambdaIntegration('ImportProductsFileLambdaIntegration', importProductsFile)
 });
 
-// api.addRoutes({
-//   path: `/test`,
-//   methods: [apiGateway.HttpMethod.GET],
-//   integration: new HttpLambdaIntegration('AuthTestLambdaIntegration', authTest),
-//   authorizer
-// });
+api.addRoutes({
+  path: `/test`,
+  methods: [apiGateway.HttpMethod.GET],
+  integration: new HttpLambdaIntegration('AuthTestLambdaIntegration', authTest)
+});
+
+const authorizer = new apiGateway.HttpAuthorizer(stack, 'BasicHttpAuthorizer', {
+  authorizerName: 'auth-test',
+  type: apiGateway.HttpAuthorizerType.LAMBDA,
+  httpApi: api,
+  identitySource: ['request.header.Authorization'],
+  authorizerUri: `arn:aws:apigateway:${process.env.PRODUCT_AWS_REGION}:lambda:path/2015-03-31/functions/${process.env.AUTHORIZER_FUNCTION_ARN!}/invocations`,
+});
 
 new cdk.CfnOutput(stack, 'ApiUrl', {
   value: `${api.url}${BASE_URL}`,
